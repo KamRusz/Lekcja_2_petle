cbeadcda91247f308698686d807bf9ab9ba20bb1
Kamil <kamilrusztyn@gmail.com>
2021-12-27 16:46:35 +0100

    poprawki z warsztatów - działający program

--- ladowarka.py
+++ ladowarka.py
@@ -3,6 +3,8 @@ packages = 0            #liczba paczek wysłanych
 kilos = 0               #suma kilogramów wysłanych
 weight = 0.0            #waga elementu
 box_weight = 0.0        #waga paczki
+max_empty_box_weight = 0
+max_empty_box_number = 0
 print ('ładowarka paczek v0.9')
 print ('podaj liczbę elementów do wysyłki')
 items = int (input())
@@ -10,20 +12,32 @@ while items > 0:
     print ('podaj wagę elementu')
     weight = float (input())
     if (weight == 0 or weight > 10 or weight < 1):
-        break
-    else:    
-        if box_weight <= 20:
-            box_weight = box_weight+weight 
+        break   
+    if box_weight + weight <= 20:
+            box_weight += weight 
             kilos += weight
             print (kilos)
-        else:  
-            box_weight= 0 + weight 
-            packages += 1
+    else:  
+        empty_box_weight = 20 - box_weight
+        box_weight = 0 
+        box_weight= weight 
+        kilos += weight
+        packages += 1
+        print (kilos)
+        if empty_box_weight > max_empty_box_weight:
+            max_empty_box_number = packages
+            max_empty_box_weight = empty_box_weight
     items -= 1
-if weight > 10 or weight < 1:
+if weight > 10 or (weight < 1 and weight!= 0):
     print ('błąd')
 else:
+    if box_weight:
+        empty_box_weight = 20 - box_weight
+        packages += 1
+        if empty_box_weight > max_empty_box_weight:
+            max_empty_box_number = packages
+            max_empty_box_weight = empty_box_weight
     print ('liczba paczek wysłanych {}' .format(packages))
     print ('liczba kilogramów wysłanych {}' .format(kilos))
     print ('suma pustych kilogramów {}' .format(packages * 20 - kilos))
-    #print ('paczka numer {} miała najwięcej pustych kilogramów - {}kg' .format(box,20-box_weight))
+    print ('paczka numer {} miała najwięcej pustych kilogramów - {}kg' .format(max_empty_box_number ,max_empty_box_weight))
